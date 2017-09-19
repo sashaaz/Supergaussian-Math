61547fe70c1e895d61d3cca292a24f22a072512a
sashaaz <faerieixi@gmail.com>
2017-09-19 14:33:38 -0500

    corrected analytical part of code, made more fair comparison to Peter's part.
    Works completely now.

--- flux_checking.m
+++ flux_checking.m
@@ -1,26 +1,35 @@
-%n = 1;
 d_fwhm = 100e-6;
-%fun = {};
 
-%for n = 1:100
-%    fun{n} = @(x) x/(d_fwhm)^2 .* exp(-(2*x/d_fwhm).^(2*n) * log(2));
-%    fun{n} = integral(fun{n}, 0, Inf);
-%end
-%fun_values = cell2mat(fun);
+%% Analytical method of checking.
+fun = {};
 
-%for n = 1:100
-%    A0(n) = sqrt(gamma(1/n)/(n*log(2)^(1/n)));
-%end
+for n = 1:100
+   fun{n} = @(x) x/(d_fwhm)^2 .* exp(-(2*x/d_fwhm).^(2*n) * log(2));
+   fun{n} = integral(fun{n}, 0, Inf);
+end
+fun_values = cell2mat(fun);
 
-%plot(A0)
-%hold on
-%plot(fun_values + 1 - .13);
-%legend('Integral of x * exp(-(2*x/d_{fwhm})^{2n} * log(2))', 'Dependence of 1/A0');
-%xlabel('n');
+for n = 1:100
+   A0(n) = sqrt(n*log(2)^(1/n)/(gamma(1/n)));
+end
+P0 = abs(A0).^2 .* fun_values;
+plot(n, P0);
+xlabel('n');
+title('P0 - i.e. |A0|^2 * integral (should be constant)');
 
+figure
+plot(0.125*abs(A0).^(-2))
+hold on
+plot(fun_values)
+title('Curve of n-dependence for .125/|A0|^2 and the integral - should be the same');
+legend('.125/|A0|^2', 'integral');
+
+
+%% Numerical Method of checking
+clear all
 
 x = d_fwhm*(0.01 : 0.01 : 100);
-dx = x(2) - x(1)
+dx = x(2) - x(1);
 for n = 1:100
     r_0 = d_fwhm / 2 / (log(2)).^(1/2/n);
     f_x = exp(-(x / r_0).^(2*n)) .* x;
